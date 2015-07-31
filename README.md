3cf25d52fc3ed0e8d2b903e3f815d2e42391e004
jinxingxing <jxinging@gmail.com>
2015-07-31 15:55:22 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-CDNZZ APIv3 PHP SDK
+速致 CDN PHP SDK APIv3
 ====================
+- 使用示例参考: tests.php
+- API文档 <https://docs.cdnzz.com/dev/api-v3/>
+
--- src/config.inc.php
+++ src/config.inc.php
@@ -1,4 +1,5 @@
 <?php
 
-define('API_URL', "https://www.cdnzz.com/apiv3/json");
+//define('API_URL', "https://www.cdnzz.com/apiv3/json");
+define('API_URL', "http://127.0.0.1:18888/apiv3/json");
 define('INVALID_TOKEN_ERROR', 10260);
